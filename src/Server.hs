455088dbbb5a4925f3ce14e41d4a5f1baee5b9cb
wfaler <wille.faler@gmail.com>
2013-01-25 13:06:23 +0000

    extracting params & headers

--- src/Server.hs
+++ src/Server.hs
@@ -1,13 +1,31 @@
 {-# LANGUAGE OverloadedStrings #-}
 import Network.Wai
-import Network.HTTP.Types (status200)
+import Network.HTTP.Types 
 import Network.Wai.Handler.Warp (run)
 import Data.ByteString.Lazy as LBS
+import Data.ByteString as BS
+import Data.ByteString.Char8 as CS
 
 app :: Application
-app req = return $ responseLBS status200 [("Content-Type", "text/plain")] (LBS.fromChunks $ [rawPathInfo req])
+app req = do
+	return $ responseLBS status200 [("Content-Type", "text/plain"), ("Set-Cookie", "haskell=foobar;Path=/")] (mapAppRequest (requestHeaders req) (queryString req))
 --	"/ok" if(requestMethod req == ) -> return $ responseLBS status200 [("Content-Type", "text/plain")] "OK"
 --	"/ok/sortof" -> return $ responseLBS status200 [("Content-Type", "text/plain")] (show $ requestMethod req)
+--	path <- rawPathInfo req
+--	method <- requestMethod req
+--	headers <- requestHeaders req
+--	queryParams <- queryString req
+
+mapAppRequest :: RequestHeaders -> Query -> LBS.ByteString
+mapAppRequest headers queryParams = (strictsToLazyBs $ (fmap (CS.pack . show) headers) ++ (fmap (CS.pack . show) queryParams))
+
+strictToLazyBs :: BS.ByteString -> LBS.ByteString
+strictToLazyBs bs = LBS.fromChunks [bs]
+
+strictsToLazyBs :: [BS.ByteString] -> LBS.ByteString
+strictsToLazyBs bs = LBS.fromChunks bs
+
+
 
 application _ = return $
   responseLBS status200 [("Content-Type", "text/plain")] "Hello World"
