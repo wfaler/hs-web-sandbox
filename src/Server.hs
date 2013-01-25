faa6f373d0747b667b545408d70816edf46866bf
wfaler <wille.faler@gmail.com>
2013-01-25 10:52:25 +0000

    added bytestring dependency to be able to move between lazy and strict@

--- src/Server.hs
+++ src/Server.hs
@@ -2,8 +2,14 @@
 import Network.Wai
 import Network.HTTP.Types (status200)
 import Network.Wai.Handler.Warp (run)
+import Data.ByteString.Lazy as LBS
+
+app :: Application
+app req = return $ responseLBS status200 [("Content-Type", "text/plain")] (LBS.fromChunks $ [rawPathInfo req])
+--	"/ok" if(requestMethod req == ) -> return $ responseLBS status200 [("Content-Type", "text/plain")] "OK"
+--	"/ok/sortof" -> return $ responseLBS status200 [("Content-Type", "text/plain")] (show $ requestMethod req)
 
 application _ = return $
   responseLBS status200 [("Content-Type", "text/plain")] "Hello World"
 
-main = run 3000 application
+main = run 3000 app
